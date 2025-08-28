c11b7fd02ea34584de6f07b914fb654a366f20e0
Fatima Alhusain <justanlaptop12@gmail.com>
2025-08-28 21:37:06 +0300

    Add files via upload

---
+++ BlogDataLibrary/Data/SalData.cs
@@ -0,0 +1,17 @@
+﻿using BlogDataLibrary.Database;
+
+namespace BlogDataLibrary.Data
+{
+    public class SqlData
+    {
+        private readonly ISqlDataAccess db;
+        private readonly string connectionStringName = "Default";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            this.db = db;
+        }
+
+        // You’ll add methods here later to interact with Users and Posts
+    }
+}
